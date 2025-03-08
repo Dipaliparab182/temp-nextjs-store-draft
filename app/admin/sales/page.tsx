7d4b7c796cf191d8aad792559e016ed6b589440f
Dipali Parab <dipaliparab182@gmail.com>
2025-03-08 15:59:09 +0100

    third commit-image validation

---
+++ app/admin/sales/page.tsx
@@ -0,0 +1,4 @@
+function SalesPage() {
+  return <div>Sales Page</div>
+}
+export default SalesPage
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,25 @@
+'use server'
 import db from '@/utils/db'
+
 import { redirect } from 'next/navigation'
+import { auth, currentUser } from '@clerk/nextjs/server'
+import { productSchema, imageSchema, validateZodWithSchema } from './schemas'
+
+const renderError = (error: unknown): { message: string } => {
+  console.log(error)
+  return {
+    message: error instanceof Error ? error.message : 'An error occurred',
+  }
+}
+
+const getAuthUser = async () => {
+  const user = await currentUser()
+  if (!user) {
+    throw new Error('You must be logged in to access this route')
+  }
+  return user
+}
+
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
     where: {
@@ -31,4 +51,30 @@ export const fetchSingleProduct = async (productId: string) => {
   })
   if (!product) redirect('/products')
   return product
+}
+
+export const createProductAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser()
+
+  try {
+    const rawData = Object.fromEntries(formData)
+    const file = formData.get('image') as File
+    const validatedFields = validateZodWithSchema(productSchema, rawData)
+    const validatedFile = validateZodWithSchema(imageSchema, { image: file })
+    console.log(validatedFile)
+
+    await db.product.create({
+      data: {
+        ...validatedFields,
+        image: '/images/product-3.jpg',
+        clerkId: user.id,
+      },
+    })
+    return { message: 'product created' }
+  } catch (error) {
+    return renderError(error)
+  }
 }
